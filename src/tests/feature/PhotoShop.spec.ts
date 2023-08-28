ca85d99161d8301cfe287f33da647375596e4b40
kphan <khoa.phan@agoda.com>
2023-08-28 10:41:04 +0700

    Add comments

--- src/tests/feature/PhotoShop.spec.ts
+++ src/tests/feature/PhotoShop.spec.ts
@@ -7,8 +7,12 @@ test.describe('Adobe PhotoShop Page Tests', () => {
 	test('Verify "Buy Now" links on the page have a href', async ({ page }) => {
 		await setupPage(page);
 
+		// TODO replace timeout with an assertion of a component that is guaranteed to be on the page
 		await page.waitForTimeout(5000);
 
+		// Alternative, could have gotten the text by "Buy now" and then used that to get the href
+		// Didn't want to click on the link because it would take me to a different page
+		// Maybe can just make a http request to the href and check the status code
 		for (const buyNowLink of await page.locator('a[daa-ll="Buy now"]').all()) {
 			if (await buyNowLink.isVisible()) {
 				expect(buyNowLink).toHaveAttribute('href', buyNowHrefRegex);
--- src/tests/visual/PhotoShop.spec.ts
+++ src/tests/visual/PhotoShop.spec.ts
@@ -5,10 +5,13 @@ test.describe('Adobe PhotoShop Page Tests', () => {
 	test('Visual regression of each section', async ({ page }, textInfo) => {
 		await setupPage(page);
 
+		// TODO replace timeout with an assertion of a component that is guaranteed to be on the page
 		await page.waitForTimeout(5000);
 
 		const pageSections = await page.locator('div[id="root_content_position"] > div > div');
 
+		// Note: Faced a bug on iPhone XR where it couldn't take a screenshot of a large component
+		// So workaround is to just take a screenshot of the whole page
 		if (textInfo.project.name === 'iPhone XR') {
 			if (await pageSections.isVisible) {
 				await expect(await page.screenshot({ fullPage: true })).toMatchSnapshot();
